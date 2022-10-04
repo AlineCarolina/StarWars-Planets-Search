83ecfdbce444a36c866c9ef449116250a21f5a57
Aline <eualine2017@gmail.com>
2022-10-04 20:09:32 -0300

    feat: change dataprovider

--- src/context/DataProvider.js
+++ src/context/DataProvider.js
@@ -63,3 +63,70 @@ DataProvider.propTypes = {
 };
 
 export default DataProvider;
+
+/* import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+import DataContext from './DataContext';
+import FetchApi from '../services/FetchApi';
+
+const DataProvider = ({ children }) => {
+  const INITIAL_FILTERS = {
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [],
+  };
+
+  const NUMERIC_FILTER = {
+    column: '',
+    comparison: '',
+    value: '',
+  };
+
+  const [filter, setFilter] = useState(INITIAL_FILTERS);
+  const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState(planets);
+  const [numericFilter, setNumericFilter] = useState(NUMERIC_FILTER);
+  const [selectedColumn, setSelectedComun] = useState();
+  const { filterByName: { name } } = filter;
+
+  useEffect(() => {
+    const fetchPlanet = async () => {
+      const result = await FetchApi();
+      setPlanets(result);
+    };
+    fetchPlanet();
+  }, []);
+
+  useEffect(() => {
+    const filtereds = planets
+      .filter((planet) => planet.name.includes(name.toLowerCase()));
+    setFilteredPlanets(filtereds);
+  }, [name, planets]);
+
+  const contextValue = {
+    filter,
+    setFilter,
+    planets,
+    setPlanets,
+    filteredPlanets,
+    setFilteredPlanets,
+    numericFilter,
+    setNumericFilter,
+    selectedColumn,
+    setSelectedComun,
+  };
+
+  return (
+    <DataContext.Provider value={ contextValue }>
+      { children }
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DataProvider;
+ */
